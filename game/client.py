917f49cc3429b962b514be077280910e4a44a7c8
Nick Roberts <nroberts@andrew.cmu.edu>
2017-04-24 13:01:07 -0400

    Allows client to receive data too

--- game/client.py
+++ game/client.py
@@ -4,18 +4,38 @@
 
 import socket
 import pickle
+from threading import Thread
+
+thread_continuing = True
 
 def main():
     client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     client.connect(("localhost", 8081))
-    while True:
-        x = raw_input()
-        if x == "1":
-            obj = ("swing", { "player": 1 })
-        else:
-            obj = ("swing", { "player": 2 })
-        data = pickle.dumps(obj)
-        client.send(data)
+
+    def write():
+        global thread_continuing
+        while thread_continuing:
+            x = raw_input()
+            if x == "1":
+                obj = ("swing", { "player": 1 })
+            else:
+                obj = ("swing", { "player": 2 })
+            data = pickle.dumps(obj)
+            client.send(data)
+
+    def read():
+        global thread_continuing
+        while thread_continuing:
+            x = client.recv(1024)
+            print pickle.loads(x)
+
+    write_thread = Thread(target=write)
+    write_thread.start()
+    try:
+        read()
+    finally:
+        global thread_continuing
+        thread_continuing = False
 
 if __name__ == "__main__":
     main()
