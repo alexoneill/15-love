ed488953ff1d165ceb173129c025ef053499e43c
Alex O'Neill <git@aoneill.me>
2017-04-26 08:57:08 -0400

    Removed static ip in favor of environment config
    This applies to the racket client application

--- rackets/__main__.py
+++ rackets/__main__.py
@@ -1,6 +1,7 @@
 # __main__.py
 # aoneill - 04/24/17
 
+import os
 import socket
 import sys
 
@@ -10,19 +11,31 @@ from src import text_racket
 # Main method when running the racket driver code
 def main(typ = 'game'):
   if(typ == 'game'):
+    if(('PB_15LOVE_IP' not in os.environ) or
+        ('PB_15LOVE_IP' not in os.environ)):
+      print 'Missing PB_15LOVE_IP or PB_15LOVE_PORT!'
+      return 1
+
+    # Get server parameters
+    ip = os.environ['PB_15LOVE_IP']
+    port = os.environ['PB_15LOVE_PORT']
+
+    # Connect to the server
     client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    # client.connect(('pbridge.adm.cs.cmu.edu', 8081))
-    client.connect(('128.2.208.126', 8081))
+    client.connect((ip, int(port)))
 
     # Get a text-based racket and constantly update it
     gracket = game_racket.GameRacket(client, 2)
     while(True):
       gracket.update()
+
   elif(typ == 'text'):
     # Get a text-based racket and constantly update it
     tracket = text_racket.TextRacket()
     while(True):
       tracket.update()
 
+  return 0
+
 if(__name__ == '__main__'):
-  main(*sys.argv[1:])
+  sys.exit(main(*sys.argv[1:]))
