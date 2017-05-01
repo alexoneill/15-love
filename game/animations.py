055fd5aacaac4ffe2dbd6ae845f394344b0c44be
Nick Roberts <nroberts@andrew.cmu.edu>
2017-04-30 21:25:47 -0400

    Tweaks constants

--- game/animations.py
+++ game/animations.py
@@ -53,7 +53,7 @@ class Animation(object):
 
 # Bars for displaying score to each other
 class ScoreBars(Animation):
-    FADE_FRAMES = 120 # how long to hang at the end
+    FADE_FRAMES = 160 # how long to hang at the end
     FLASH_CYCLE = 40 # how long to fade in and out
 
     def init(self, end_seq):
@@ -163,8 +163,8 @@ class ScoreAnimation(object):
     def make_scorebar_animations(self):
         animations = []
         p1, p2 = self.p1, self.p2
-        p1_offset = p1.origin + p1.max_swing - 6 * ScoreAnimation.SEPARATION
-        p2_offset = p2.origin - p2.max_swing + 6 * ScoreAnimation.SEPARATION
+        p1_offset = p1.origin + p1.max_swing - 4 * ScoreAnimation.SEPARATION
+        p2_offset = p2.origin - p2.max_swing + 4 * ScoreAnimation.SEPARATION
 
         # constants used when creating animations
         v = ScoreAnimation.VELOCITY
